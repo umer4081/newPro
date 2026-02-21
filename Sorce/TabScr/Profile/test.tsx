98488e0225e157a34ec54193d43768b73e760667
umer Majeed <mirumar657@gmail.com>
2026-02-21 16:58:29 +0530

    as I do create my new file

---
+++ Sorce/TabScr/Profile/test.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const test = () => {
+  return (
+    <View>
+          <Text>{ 'Test is my code upload on git or not '}</Text>
+    </View>
+  )
+}
+
+export default test
+
+const styles = StyleSheet.create({})

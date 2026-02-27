41f50b513da5b92cd5cb62a4eba4b04f8700b235
umer Majeed <mirumar657@gmail.com>
2026-02-27 06:15:31 +0530

    check message

--- Sorce/TabScr/Profile/test.tsx
+++ Sorce/TabScr/Profile/test.tsx
@@ -2,6 +2,13 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const test = () => {
+    //=========investigation start we do here
+    const accounId = 144553 //rendom number
+    let accounEmail = 'eemail@gmail.com'
+    var accoundPass = '12345',
+        accounytCity = 'Kashmir'
+    accounId = 12 // now node js will anylasie this and give us output
+    console.log(accounId, 'accounIdaccounId')
   return (
     <View>
           <Text>{ 'Test is my code upload on git or not '}</Text>
